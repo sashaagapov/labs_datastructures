7088e61227f9f180c984c44faf46ccb1bc75577f
Sasha Agapov <sasha20070315@gmail.com>
2026-02-06 17:17:17 +0200

    Renamed folders,building future main lab1 folder

---
+++ lab1_ds_futuremain/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace SearchAlgorithms
+{
+   
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 100000;
+            int[] numbers = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                numbers[i] = i;
+            }
+
+            Console.Write("Введіть шукане число: ");
+            if (!int.TryParse(Console.ReadLine(), out int target))
+            {
+                Console.WriteLine("Помилка: введіть число");
+                return;
+            }
+            Console.WriteLine($"Шукаємо число {target}...");
+
+            // Створюємо та запускаємо таймер
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+
+            // Викликаємо наш пошук
+            int index = LinearSearch(numbers, target);
+
+            // Зупиняємо таймер
+            sw.Stop();
+
+            if (index != -1)
+            {
+                Console.WriteLine($"The index of searching number is: {index}");
+            }
+            else
+            {
+                Console.WriteLine("The target wasn't found");
+            }
+
+            // Виводимо час
+            Console.WriteLine($"Час виконання лінійного пошуку: {sw.Elapsed.TotalMilliseconds} мс");
+        }
+       
+        static int LinearSearch(int[] a, int x)
+        {
+            for(int i = 0;i < a.Length;i++)
+            {
+                if (a[i] == x)
+                {
+                   return i;
+                }
+            }
+            return -1;
+        }
+
+    }
+
+}
